e9078790a0db076bae3b86ba5737004a673fe8ff
kaguaryan <ryankagua@gmail.com>
2025-06-20 11:57:59 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,9 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    length = len(arr)
+    for start_index in range(length):
+        smallest_index = start_index
+        for index in range(start_index + 1, length):
+            if arr[index] < arr[smallest_index]:
+                smallest_index = index
+        arr[start_index], arr[smallest_index] = arr[smallest_index], arr[start_index]
+    return arr
